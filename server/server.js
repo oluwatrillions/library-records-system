c8ef6b9ca5104c4b89c785939d00c6eb888ca81e
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-04-06 23:50:50 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+
+const PORT = 5000
+
+const db = 
+
+
+app.post('/register', (req, res) => {
+    
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port${PORT}`
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [address, setAddress] = useState('')
+  const [age, setAge] = useState(0)
+  const [book, setBook] = useState('')
+  const [date, setDate] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="info">
+        <h1>Library Record System</h1>
+            <div class="details">
+                <label for="name">Name</label>
+                <input type="text" onChange={(e)=>{
+                  setName(e.target.value)}}/>
+            </div>
+            <div class="details">
+                <label for="address">Address</label>
+                <input type="text" onChange={(e)=>{
+                  setAddress(e.target.value)}}/>
+            </div>
+            <div class="details">
+                <label for="age">Age</label>
+                <input type="number" onChange={(e)=>{
+                  setAge(e.target.value)}}/>
+            </div>
+            <div class="details">
+                <label for="bookOfChoice">Book of Choice</label>
+                <input type="text" onChange={(e)=>{
+                  setBook(e.target.value)}}/>
+            </div>
+            <div class="details">
+                <label for="returnDate">Date</label>
+                <input type="date" onChange={(e)=>{
+                  setDate(e.target.value)}}/>
+            </div>
+            <button>Submit</button>
+      </div>
     </div>
   );
 }
